85622b22648c1968a7d24cebd4138bc03614a0f5
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-09-07 06:10:10 +0530

    conditional_statement_loop Please enter the commit message for your changes. Lines starting

--- conditional_statement_loop.cpp
+++ conditional_statement_loop.cpp
@@ -1,11 +1,125 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n = -4;
+/* int main(){
+    int n = 34;
     if(n>=0){
         cout << "n is positive\n";
 
     }
+    else{
+        cout<<"n is negative";
+    }
     return 0;
-}
+} */
+/* input from the user */
+/* int main(){
+    int age;
+    cout<<"enter the name";
+    cin>>age;
+    if(age <= 18){
+        cout<<"a person cannnot vote";
+    }
+    else{
+        cout<<"a person can vote";
+    }
+    return 0;
+} */
+
+
+/* else if  = when we have multiple condition to check */
+/* greade system */
+/* int main(){
+    int num;
+    cout << "enter the number";
+    cin >> num;
+    if(num >= 80)
+        cout<<"first class pass";
+    }
+    else if (num>=50)
+    {
+        cout<<"second class";
+    }
+    else{
+        cout<<"third class";
+        
+    }
+    
+
+} */
+/* 
+ternary statement  = if else ki condition ko ek line me likhna */
+/* int main(){
+    int n = -45;
+    cout << (n >=0? "positive" : "negative") << endl;
+    return 0;
+} */
+
+
+
+
+/* 
+Looop */
+
+
+
+
+/* 
+while loop */
+/* 
+int main()
+{
+    int count = 1;
+    while ((count <= 5))
+    {
+        cout << count << " ";
+        count++;
+    }
+    cout << endl;
+    return 0;
+} */
+/* 
+infinite loop =  a loop that cannot end it run continuesly  */
+/* 
+for loop = for(initialisation;condition;updation)
+{
+    work flow
+} */
+/* 
+int main (){
+    for(int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+} */
+/* int main (){
+    int sum = 0;
+    int n;
+    int n = 23;
+
+    for(int i = 1; i <= n; i++){
+        if(i % 2 != 0){
+            sum += i;
+    }
+    }
+    cout << sum << endl;
+
+} */
+/* 
+break statement  = to exit the loop */
+/* q = sum of alll odd number 1 to n */
+
+/* int main(){
+    int sum = 0;
+    int n;
+    cout << "enter the number";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        if(i % 2 != 0){
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+} */
