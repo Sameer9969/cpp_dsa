26b4fbf65219471df401c5c1bf0052a560c1b4e3
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-09-17 05:29:01 +0530

    cpp

---
+++ function_leacture5.cpp
@@ -0,0 +1,31 @@
+/* function syntax */
+#include <iostream>
+using namespace std;
+
+/* a function that does not return any thing is called void */
+
+
+/* function definition */
+/* void print_hello() {
+    cout<<"hello\n";
+}
+
+int main(){ */
+    /* function call */
+    /* print_hello();
+
+    return 0; */
+
+
+    /* parameterised function */
+int sum(int a ,int b){
+    int s = a+b;
+    return s;
+}
+
+int main(){
+    cout<<sum(10,5) << endl;
+    return 0;
+}
+
+
